ec4e32b38884fb45624597012264e8c15514ac93
juanfel254 <jparrar@unal.edu.co>
2023-02-26 13:40:46 -0500

    First Pokemon API succesfull consumption

--- src/components/Pokemons/Pokemons.jsx
+++ src/components/Pokemons/Pokemons.jsx
@@ -1,15 +1,20 @@
 import PokemonCard from "./PokemonCard/PokemonCard.jsx";
 import "./Pokemons.css";
 
-export default function Pokemons() {
-	return (
-		<ul className="cards-container">
-			<PokemonCard 
-				src="./imgs/001.png"
-				pokemonName="Bulbasaur"
-				pokemonNumber="001"
-				pokemonType="grass"
-			/>
-		</ul>
-	)
+export default function Pokemons({ pokemonsData } ) {
+
+    let pokemons = undefined;
+    if (pokemonsData.length) {
+      pokemons = pokemonsData.map( pokemon => {
+        return <PokemonCard 
+          pokemonName={pokemon.name}
+          key = {pokemon.url}
+        />
+      })
+    }
+    
+    return (
+      <ul className="cards-container">
+        {pokemons}
+      </ul>);
 }
