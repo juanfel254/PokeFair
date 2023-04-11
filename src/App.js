42d458fc63ed29e0a2b4a4d7bf65aa45237f36b9
Juan Felipe Parra R <jparrar@unal.edu.co>
2023-04-11 15:18:56 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import Header from './components/Header/Header.jsx';
-import Pokemons from './components/Pokemons/Pokemons.jsx';
-import './App.css';
-import React, { useState } from 'react';
+import Header from "./components/Header/Header.jsx";
+import Pokemons from "./components/Pokemons/Pokemons.jsx";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 async function getPokemons() {
-  const response = await fetch (
-    "https://pokeapi.co/api/v2/pokemon/",
-    {
-      method: "GET",
-    }
-  );
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/", {
+    method: "GET",
+  });
   const data = await response.json();
   return data.results;
 }
 
 function App() {
+  let [pokemonsData, setPokemonsData] = useState([]);
+  /*   if (!pokemonsData.length) {
+    getPokemons().then((pokemons) => setPokemonsData(pokemons));
+  } */
 
-  let [pokemonsData, setPokemonsData] = useState([]); 
-  if (!pokemonsData.length){
-    getPokemons().then(pokemons => (setPokemonsData(pokemons)));
-  }
+  useEffect(() => {
+    getPokemons().then((pokemons) => setPokemonsData(pokemons));
+  }, []);
 
   return (
     <div className="App">
       <Header />
       <main>
-        <Pokemons pokemonsData={pokemonsData}/>
+        <Pokemons pokemonsData={pokemonsData} />
       </main>
     </div>
   );
